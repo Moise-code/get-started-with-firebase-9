1b7d81cf0588c99d5881a9cf56fad1efe7088fc9
Moise-code <moisewowe@gmail.com>
2023-01-06 12:00:26 +0200

    adding the functionality for the update form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { initializeApp} from 'firebase/app';
-import { getFirestore, collection, onSnapshot, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import { getFirestore, collection, onSnapshot, addDoc, deleteDoc, doc, query, where, orderBy, serverTimestamp, getDoc, updateDoc } from 'firebase/firestore';
 
 const addBookForm = document.querySelector('.add');
 const deleteBookForm = document.querySelector('.delete');
+const updateForm = document.querySelector('.update')
 
 const firebaseConfig = {
     apiKey: "AIzaSyCTUrZs6gMDcWs3BU_R77Xnt8nj1i0lqgQ",
@@ -24,27 +25,26 @@ const db = getFirestore();
 // collection reference or a database 
 const colRef = collection(db, 'books')
 
+// querries
+const q = query(colRef, orderBy('createdAt'));
 
 // get collection data from the collection as real time data.
 
-onSnapshot(colRef,(snapshot) =>{
+onSnapshot(q, (snapshot) =>{
     let books = [];
     snapshot.docs.forEach((doc) =>{
      books.push({...doc.data(), id: doc.id })
     })
     console.log(books);
  })
- .catch((error) =>{
-     console.log(error);
-
-})
 // adding the event listerner to the add book form
 
 addBookForm.addEventListener('submit', (e) =>{
     e.preventDefault();
     addDoc(colRef, {
         title: addBookForm.title.value.trim(),
-        author: addBookForm.author.value.trim()
+        author: addBookForm.author.value.trim(),
+        createdAt: serverTimestamp()
     })
     .then(() =>{
 addBookForm.reset();
@@ -63,4 +63,28 @@ deleteBookForm.addEventListener('submit', (e) =>{
     }).catch((error) =>{
         console.log(error);
     });
+})
+
+// get a single document
+
+const docRef = doc(db,'books','eQiJo9tH8diMHT9VOExg');
+getDoc(docRef)
+.then((doc) =>{
+    console.log(doc.data(), doc.id);
+})
+
+// get the updates from the firestore updated on the snapshot view.
+onSnapshot(docRef,(doc) =>{
+    console.log(doc.data(), doc.id)
+})
+
+updateForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const docRef = doc(db,'book', updateForm.id.value.trim());
+    updateForm(docRef, {
+        title: 'update title' 
+    })
+    .then(() =>{
+        updateForm.reset();
+    })
 })
